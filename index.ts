62a711117d7ae0440f667eb8013b6f73b358d697
unknown <particular6@proton.me>
2025-02-14 22:27:24 +0500

    added filtering before sorting, searching for a title

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import router from "./src/auth/authRoutes";
-import { getAllItems, sortItemsByPrice } from "./src/items/itemsServices";
+import { getAllItems, sortItemsByPrice, searchForSingleItem } from "./src/items/itemsServices";
 
 const app = express();
 
@@ -12,10 +12,14 @@ app.use(router);
 const PORT = process.env.PORT || 3001;
 
 app.get('/', async () => {
-    const items = await getAllItems();
-    const sorted = sortItemsByPrice(items, false);
-    console.log(`hello there`);
-    console.log(sorted);
+    // const items = await getAllItems();
+    try {
+        const res = await searchForSingleItem('iPhone ');
+        console.log(`hello there`);
+        console.log(res);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 app.listen(PORT, () => {
--- src/items/itemsServices.ts
+++ src/items/itemsServices.ts
@@ -17,27 +17,49 @@ export const getAllItems = async function () {
         console.error(err);
         throw err;
     } finally {
-        // await disconnectDB();
+        await disconnectDB();
     }
 }
+
+export const searchForSingleItem = async function (itemName: string) {
+    try {
+        await connectDB();
+        const query = "SELECT * FROM items WHERE name ILIKE $1 ";
+        const foundItems = await pool.query(query, [`%${itemName}%`]);
+        // console.log(foundItems.rows);
+        return foundItems.rows
+    } catch (err) {
+        console.error(err);
+        throw err;
+    } 
+    // finally {
+    //     await disconnectDB();
+    // }
+}
+
+
 
 // пока не создаю полноценный интерфейс, потому что меня будет интересовать одно конкретное поле. 
 // собственно, суть в том, что первым параметром функция принимает массив объектов, у которых будет поле price
 export const sortItemsByPrice = function<T extends ItemWithPrice>(items: T[], cheaperFirst: boolean = true) {
+    const onlyElementsWithPrice = items.filter( (item) => {
+        return item.price !== undefined;
+    })
+
     if (cheaperFirst) {
-        items.sort( (a,b) => {
+        onlyElementsWithPrice.sort( (a,b) => {
             const parsedA = parseFloat(a.price);
             const parsedB = parseFloat(b.price);
             return parsedA - parsedB;
         })
     }
     else {
-        items.sort( (a,b) => {
+        onlyElementsWithPrice.sort( (a,b) => {
             const parsedA = parseFloat(a.price);
             const parsedB = parseFloat(b.price);
             return parsedB - parsedA;
         })
     }
 
-    console.log(items);
+    console.log(onlyElementsWithPrice);
 }
