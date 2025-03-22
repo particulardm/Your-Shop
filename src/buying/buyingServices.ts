da20bdf5d6146d1209c915a6e9053b64d7a6094e
unknown <particular6@proton.me>
2025-03-22 23:33:21 +0500

    minor refactoring 2

--- src/buying/buyingServices.ts
+++ src/buying/buyingServices.ts
@@ -38,8 +38,6 @@ export const buy = async function (req: Request, res: Response) {
                         discountValue = 50;
                     };
                 }
-                // теперь надо подогнать логику под расчёт скидки по-разному в зависимости от дискаунт тайпа
-                // я могу задать дефолтный дискаунт тайп как абсолютный и просто менять его здесь, если потребуется
             }
         }
         await buyCurrentCart(username, discountType, discountValue, couponID);
@@ -55,24 +53,12 @@ export const buy = async function (req: Request, res: Response) {
 };
 
 const buyCurrentCart = async function (username: string, discountType: "absolute" | "percent", discountValue: number, couponID?: number) {
-    // запрашиваем корзину для ЮЗЕРНЕЙМ из базы
-    // const searchCartQuery = "SELECT * FROM carts WHERE user_name = $1";
-    // const searchCartResult = await pool.query(searchCartQuery, [ username ]);
-    // смотрим, чтобы была не пустая
     const userCart = await getUserCart(username);
     if (userCart.length < 1) {
         throw new Error("can't buy an empty cart..");
     }
 
     let totalPrice = await calculateTotalPrice(userCart);
-    // for (const item of userCart) {
-    //     const searchItemQuery = "SELECT * FROM items WHERE id = $1";
-    //     const searchItemResult = await pool.query(searchItemQuery, [ item["item_id"] ]);
-
-    //     totalPrice += searchItemResult.rows[0].price * item.quantity;
-    // }
-    
-   
 
     // дефолтно у скидки абсолютный дискаунт тайп, так что просто приравниваем её к переданному вэлью
     // если передан персент тайп, то сразу корректируем
@@ -86,38 +72,15 @@ const buyCurrentCart = async function (username: string, discountType: "absolute
     console.log("total price AFTER discount for user of ", username, " is ", totalPrice);
     // смотрим, чтобы было достаточно денег
     await checkIfEnoughMoney(username, totalPrice);
-    // const searchUserQuery = "SELECT * FROM users WHERE username = $1";
-    // const searchUserResult = await pool.query(searchUserQuery, [ username ]);
-    // if (searchUserResult.rows[0].deposit < totalPrice) {
-    //     throw new Error("not enough money to buy items");
-    // }
     // списываем средства
     await chargeUser(username, totalPrice);
-    // const updateUserDepositQuery = "UPDATE users SET deposit = deposit - $1 WHERE username = $2";
-    // const updateUserDepositResult = await pool.query(updateUserDepositQuery, [ Math.floor(totalPrice), username ]);
     // сохраняем транзакцию в историю 
     await savePurchaseToHistory(username, totalPrice, userCart);
-    // const saveWholePurchaseQuery = "INSERT INTO purchases (username, total_amount) VALUES ($1, $2) RETURNING purchase_id";
-    // const saveWholePurchaseResult = await pool.query(saveWholePurchaseQuery, [ username, totalPrice ]);
-    // console.log("the purchase is saved to db..");
-    // console.log("returns:", saveWholePurchaseResult.rows[0]["purchase_id"]);
-
-    // const saveSingleItemPurchaseQuery = `INSERT INTO purchase_items (purchase_id, price, item_id, quantity) VALUES (${saveWholePurchaseResult.rows[0]["purchase_id"]}, 1, $1, $2)`;
-    // for (const item of userCart) {
-    //     const saveSingleItemPurchaseResult = await pool.query(saveSingleItemPurchaseQuery, [ item["item_id"], item.quantity ]);
-    // };
-    // console.log("the purchases items are saved to db as well..");
     // запоминаем, что купон был использован
-    // const addCouponUsageQuery = "INSERT INTO coupon_usage (coupon_id, username, discount_applied) VALUES ($1, $2, $3)";
-    // const addCouponUsageResult = await pool.query(addCouponUsageQuery, [ couponID, username, totalDiscount ]);
     if (couponID) {
         await saveCouponUseToHistory(username, totalDiscount, couponID);
     }
-
     // обнуляем корзину
-    // const clearCartQuery = "DELETE FROM carts WHERE user_name = $1";
-    // const clearCartResult = await pool.query(clearCartQuery, [ username ]);
-
     await clearUserCart(username);
 }
 
@@ -162,7 +125,6 @@ const savePurchaseToHistory = async (username: string, totalPrice: number, userC
     const saveWholePurchaseQuery = "INSERT INTO purchases (username, total_amount) VALUES ($1, $2) RETURNING purchase_id";
     const saveWholePurchaseResult = await pool.query(saveWholePurchaseQuery, [ username, totalPrice ]);
     console.log("the purchase is saved to db..");
-    console.log("returns:", saveWholePurchaseResult.rows[0]["purchase_id"]);
 
     const saveSingleItemPurchaseQuery = `INSERT INTO purchase_items (purchase_id, price, item_id, quantity) VALUES (${saveWholePurchaseResult.rows[0]["purchase_id"]}, 1, $1, $2)`;
     for (const item of userCart) {
